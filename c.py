d4ae4edd5c9ef01d63f14c78bf634c173dad7a24
Zach Forrest <ZachForrest@Zachs-MacBook-Pro.local>
2020-02-11 10:08:06 -0500

    completes c.py game

--- c.py
+++ c.py
@@ -5,28 +5,37 @@
 
 import random
 
-number_of_guesses = 0
-
 print('Hey pick a number between 1 and 100 and I will try and guess it.')
-number = input()
+number = int(input('Your Number:'))
 
 guess = random.randint(1,100)
-# guess = int(guess)
+counter = 0
 
-while number_of_guesses < 10:
-    print('Is it',guess,'?')
-    number_of_guesses = number_of_guesses + 1
-    if guess > number:
-        guess -= 1
-        guess = randint(1, guess)
-    if guess < number:
-        guess += 1
-        guess = randint(guess, 100)
+while counter < 10:
+    counter += 1
+
+    # print('Is it',guess,'?')
+    # number_of_guesses = number_of_guesses + 1
+    # if guess > number:
+    #     guess -= 1
+    #     guess = randint(1, guess)
+    # if guess < number:
+    #     guess += 1
+    #     guess = randint(guess, 100)
+
+    # print(guess)
+    # print(number)
     if guess == number:
+        print('I figured it out, it is',number,'! A computer read your mind.')
         break
+    elif guess < number:
+        print('I guessed',guess,'that was too low!')
+        guess += 1
+        guess = random.randint(guess, 100)
+    elif guess > number:
+        print('I guessed',guess,'that was too high!')
+        guess -= 1
+        guess = random.randint(1, guess)
 
-if guess == number:
-    print('I figured it out, it is',number,'! A computer read your mind.')
-
-if number_of_guesses == 10:
+if counter == 10:
     print('Oh well, I guess I cannot read minds afterall.')
